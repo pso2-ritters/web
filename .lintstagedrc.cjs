12521be72c0f51d4a939057b082cfa9e11d2da00
r-sylph <resylph@gmail.com>
2023-11-13 07:42:34 +0900

    style(prettier): docsを整形対象外にした
    cjsのlint-staged設定が抜けていたので足した

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-	"*.{js,ts,vue}": ["eslint --fix", "prettier --write"],
+	"*.{js,cjs,ts,vue}": ["eslint --fix", "prettier --write"],
 	"*.{json,md,scss,yml}": ["prettier --write"],
 	"package.json": "sort-package-json",
 };
