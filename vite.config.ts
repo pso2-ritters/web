675d381aca0de7fd2ef9cae766b1fdfac00fc699
r-sylph <resylph@gmail.com>
2023-11-13 07:56:16 +0900

    build(tsconfig): import時にsrcを@this指定できるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +11,13 @@ export default defineConfig({
 	},
 	plugins: [
 		vue(),
+		tsconfigPaths(),
 		AutoImport({
 			imports: ["vitest"],
 			dts: "./src/helpers/auto-imports.d.ts",
 		}),
 	],
+	resolve: {
+		alias: [{ find: "@this", replacement: "/src" }],
+	},
 });
